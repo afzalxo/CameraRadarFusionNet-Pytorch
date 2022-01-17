Fixed loss calculation

model/architecture/retinanet.py
@@ -5,7 +5,7 @@ from torchvision.transforms import InterpolationMode
 from model.architecture.vgg import Vggmax
 
 class Retinanet(nn.Module):
-    def __init__(self, feature_size, num_values_regression, num_anchors, num_classes, image_size=(360, 640)):
+    def __init__(self, backbone, num_anchors, num_classes, num_values_regression=4, feature_size=254, image_size=(360, 640)):
         super(Retinanet, self).__init__()
         self.feature_size = feature_size
         self.num_values_regression = num_values_regression
@@ -15,11 +15,11 @@ class Retinanet(nn.Module):
         self.classification_feature_size = 256
         self.num_classes = num_classes
 
-        self.backbone = Vggmax()
+        self.backbone = backbone
         __feature_size = self.backbone._feature_sizes()
         self.p5_conv1 = nn.Conv2d(in_channels=__feature_size[-1], out_channels=self.feature_size, kernel_size=1, stride=1, padding=0)
         self.p5_conv2 = nn.Conv2d(in_channels=self.feature_size, out_channels=self.feature_size, kernel_size=3, stride=1, padding=1)
-        self.p5_upsample = transforms.Resize((int(image_size[0]/16), int(image_size[1]/16)), interpolation=InterpolationMode.NEAREST)
+        self.p5_upsample = transforms.Resize((int(image_size[0]/16+1), int(image_size[1]/16)), interpolation=InterpolationMode.NEAREST)
 
         self.p4_conv1 = nn.Conv2d(in_channels=__feature_size[-2], out_channels=self.feature_size, kernel_size=1, stride=1, padding=0)
         self.p4_conv2 = nn.Conv2d(in_channels=self.feature_size, out_channels=self.feature_size, kernel_size=3, stride=1, padding=1)
@@ -85,15 +85,19 @@ class Retinanet(nn.Module):
         for i in range(len(self.regression_ops)):
             features = self.regression_ops[i](features)
         features = torch.permute(features, (0, 2, 3, 1))
-        outputs = torch.reshape(features, (-1, num_values))
+        outputs = torch.reshape(features, (features.shape[0], -1, num_values))
+        #print('Regression Outputs Size:')
+        #print(num_values, outputs.size())
         return outputs
 
     def run_classification_submodel(self, features, num_classes):
         for i in range(len(self.classification_ops)):
             features = self.classification_ops[i](features)
         features = torch.permute(features, (0, 2, 3, 1))
-        outputs = torch.reshape(features, (-1, num_classes))
+        outputs = torch.reshape(features, (features.shape[0], -1, num_classes))
         outputs = nn.Sigmoid()(outputs)
+        #print('Classification Outputs Size:')
+        #print(num_classes, outputs.size())
         return outputs
 
     def forward(self, input):
@@ -104,7 +108,8 @@ class Retinanet(nn.Module):
             #print(feature.size())
             #res = self.run_regression_submodel(feature, 4)
             #print(res.size())
-        regression_out = torch.cat([self.run_regression_submodel(feature, 4) for feature in pyramid_features], dim=0)
-        classification_out = torch.cat([self.run_classification_submodel(feature, self.num_classes) for feature in pyramid_features], dim=0)
+        regression_out = torch.cat([self.run_regression_submodel(feature, 4) for feature in pyramid_features], dim=1)
+        classification_out = torch.cat([self.run_classification_submodel(feature, self.num_classes) for feature in pyramid_features], dim=1)
+        #self.focal_loss(regression_out, classification_out, anchors, annotations)
         return [regression_out, classification_out]
         

model/architecture/vgg.py
@@ -37,7 +37,7 @@ class Vggmax(nn.Module):
             ('block4_conv2relu', nn.ReLU(inplace=False)),
             ('block4_conv3', nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1)),
             ('block4_conv3relu', nn.ReLU(inplace=False)),
-            ('block4_mp', nn.MaxPool2d(kernel_size=2, stride=2, padding=0))
+            ('block4_mp', nn.MaxPool2d(kernel_size=2, stride=2, padding=(1,0)))
         ]))
         self.block5 = nn.Sequential(OrderedDict([
             ('block5_conv1', nn.Conv2d(in_channels=514, out_channels=512, kernel_size=3, stride=1, padding=1)),
@@ -46,15 +46,15 @@ class Vggmax(nn.Module):
             ('block5_conv2relu', nn.ReLU(inplace=False)),
             ('block5_conv3', nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1)),
             ('block5_conv3relu', nn.ReLU(inplace=False)),
-            ('block5_mp', nn.MaxPool2d(kernel_size=2, stride=2, padding=0))
+            ('block5_mp', nn.MaxPool2d(kernel_size=2, stride=2, padding=(1,0)))
         ]))
 
         self.rad_block1_pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         self.rad_block2_pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         self.rad_block3_pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        self.rad_block4_pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        self.rad_block5_pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        self.rad_block6_pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=(1,0))
+        self.rad_block4_pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=(1,0))
+        self.rad_block5_pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=(1,0))
+        self.rad_block6_pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         self.rad_block7_pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         self.global_avg_pool = nn.AdaptiveAvgPool2d((1,1)) 
 

test_retinanet.py
@@ -5,32 +5,35 @@ from model.architecture.retinanet import Retinanet
 from model.architecture.vgg import Vggmax
 from utils.config import get_config
 from data_processing.generator.crf_main_generator import create_generators
-
-feature_size = 254
-num_values_regression = 4
-num_anchors = 9
+from model import losses
+from model import anchors
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', type=str, default='./config/default.cfg')
 args = parser.parse_args()
-a = Vggmax()
+
+num_anchors = 9
+
+image_backbone = Vggmax()
 cfg = get_config(args.config)
-train_gen, _, _, _, _ = create_generators(cfg, a)
+train_generator, validation_generator, test_generator, test_night_generator, test_rain_generator = create_generators(cfg, image_backbone)
 
-num_classes = train_gen.num_classes()
-print('Num Classes:')
-print(num_classes)
+num_classes = train_generator.num_classes()
+print('Num Classes: {}'.format(num_classes))
 
-inputs, targets = train_gen[0]
-print('Input Shapes:')
-print(inputs.shape)
-print('Target Shapes:')
-print(targets[0].shape, targets[1].shape)
+inputs, targets = train_generator[0]
+print('Input Shapes: {}'.format(inputs.shape))
+print('Target Shapes: Regression {}, Classification {}'.format(targets[0].shape, targets[1].shape))
 
-a = Retinanet(feature_size, num_values_regression, num_anchors, 9)
+b = torch.tensor(inputs, dtype=torch.float)
+b = torch.permute(b, (0, 3, 1, 2))
 
-b = torch.tensor(np.ones((1, 5, 360, 640)), dtype=torch.float)
+model = Retinanet(image_backbone, num_anchors, num_classes)
+res = model(b)
+anch = anchors.Anchors()(b)
+print('Anchor Size: {}'.format(anch.size()))
 
-res = a(b)
-print('Res[0] size and Res[1] size')
-print(res[0].size(), res[1].size())
+los = losses.FocalLoss()(res[1], res[0], anch, targets[0])
+
+print('Res[0] size {} and Res[1] size {}'.format(res[0].size(), res[1].size()))
+#print(los)

utils/anchor_parameters.py
@@ -17,7 +17,7 @@ Original Source: https://github.com/fizyr/keras-retinanet
 """
 
 import numpy as np
-from keras.backend import floatx
+#from keras.backend import floatx
 #from tensorflow.keras.backend import floatx
 
 
@@ -47,13 +47,13 @@ The default anchor parameters.
 AnchorParameters.default = AnchorParameters(
     sizes   = [32, 64, 128, 256, 512],
     strides = [8, 16, 32, 64, 128],
-    ratios  = np.array([0.5, 1, 2], floatx()),
-    scales  = np.array([2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)], floatx()),
+    ratios  = np.array([0.5, 1, 2], dtype=np.float32),
+    scales  = np.array([2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)], dtype=np.float32),
 )
 
 AnchorParameters.small = AnchorParameters(
     sizes   = [16, 32, 64, 128, 256],
     strides = [8, 16, 32, 64, 128],
-    ratios  = np.array([0.5, 1, 2], floatx()),
-    scales  = np.array([2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)], floatx()),
+    ratios  = np.array([0.5, 1, 2], dtype=np.float32),
+    scales  = np.array([2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)], dtype=np.float32),
 )